b21b88bf67f237a51b13120eeff568f6bdd44774
lidyaislamiatiz <lidyaislamiati04@gmail.com>
2019-10-08 22:31:12 +0700

    mengisi file dengan property dan method

--- produk.php
+++ produk.php
@@ -1,9 +1,39 @@
 <?php
 
 class Produk {
+    public $judul = "judul",
+            $penulis = "penulis",
+            $penerbit = "penerbit",
+            $harga = 0;
 
+    public function getLabel() {
+        return "$this->penulis, $this->penerbit";
+    }
 
 }
 
-$produk1 = new Produk();
+/*$produk1 = new Produk();
+$produk1->judul = "Naruto";
+var_dump($produk1);
+
 $produk2 = new Produk();
+$produk2->judul = "Doraemon";
+$produk2->tambahProperty = "hahaha";
+
+var_dump($produk2);*/
+
+$produk3 = new Produk();
+$produk3->judul = "Naruto";
+$produk3->penulis = "Masashi";
+$produk3->penerbit = "Shoen Jump";
+$produk3->harga = 30000;
+
+$produk4 = new Produk();
+$produk4->judul = "Uncharted";
+$produk4->penulis = "Neil";
+$produk4->penerbit = "Sony";
+$produk4->harga = 70000;
+
+echo "Komik : " . $produk3->getLabel();
+echo "<br>";
+echo "Game : " . $produk4->getLabel();
